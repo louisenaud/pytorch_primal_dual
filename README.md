46d5538c556c2617b8f681e6d6266d321e1dedd4
louise <louise.naud@gmail.com>
2017-11-17 17:46:10 -0800

    Explored parameter space.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 pytorch_primal_dual
+
+This code is an experiment on considering the Primal Dual Algorithm as a Recurrent Neural Network.
+
+# Requirements
+    you can pip install of each of the following packages:
+        - png: to save images
+        - pillow: to manipulate images
+        - matplotlib: to view grpahs and images
+
+    This code heavily relies on PyTorch with CUDA backends. You can find the installer at:
+        http://pytorch.org/
+
+# Launch the demo
+        In order to launch the experiment with the parameters by defaults and the included images, run this command line in the priject directory:
+        python main.py
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import torchvision.transforms as transforms
 
 import png
 import argparse
+import time
 
 from primal_dual_model import PrimalDualNetwork, ForwardGradient, BackwardDivergence, ForwardWeightedGradient
 
@@ -26,18 +27,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run Primal Dual Net.')
     parser.add_argument('--use_cuda', type=bool, default=True,
                         help='Flag to use CUDA, if available')
-    parser.add_argument('--max_it', type=int, default=10,
+    parser.add_argument('--max_it', type=int, default=15,
                         help='Number of iterations in the Primal Dual algorithm')
-    parser.add_argument('--max_epochs', type=int, default=150,
+    parser.add_argument('--max_epochs', type=int, default=500,
                         help='Number of epochs in the Primal Dual Net')
     parser.add_argument('--lambda_rof', type=float, default=5.,
                         help='Lambda parameter in the ROF model')
-    parser.add_argument('--theta', type=int, default=0.9,
+    parser.add_argument('--theta', type=int, default=0.8,
                         help='Regularization parameter in the Primal Dual algorithm')
-    parser.add_argument('--tau', type=int, default=80,
+    parser.add_argument('--tau', type=int, default=0.01,
                         help='Parameter in Primal')
     parser.add_argument('--save_flag', type=bool, default=True,
                         help='Flag to save or not the result images')
+    parser.add_argument('--sigma_n', type=float, default=0.01,
+                        help='Noise variance to add on reference image.')
 
     args = parser.parse_args()
 
@@ -56,7 +59,7 @@ if __name__ == '__main__':
     tensor2pil = transforms.ToPILImage()
 
     # Create image to noise and denoise / train the network on
-    sigma_n = 0.1  # Noise variance
+    sigma_n = args.sigma_n  # Noise variance
     img_ = Image.open("images/image_Lena512.png")
     h, w = img_.size
     img_ref = Variable(pil2tensor(img_).type(dtype))
@@ -116,7 +119,6 @@ if __name__ == '__main__':
         gap_history.append(pe - de)
 
     f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
-
     ax1.imshow(np.array(img_))
     ax1.set_title("Reference image")
     ax2.imshow(np.array(tensor2pil(img_obs.data.cpu())).reshape(512, 512))
@@ -141,7 +143,10 @@ if __name__ == '__main__':
     noise_2 = torch.ones(img_ref_t.size())
     noise_2 = Variable(noise_2.normal_(0.0, sigma_n).type(dtype))
     img_obs_t = img_ref_t + noise
+    t_0 = time.time()
     img_dn = net.forward(img_obs_t)
+    t_1 = time.time()
+    print("Elapsed time: ", t_1 - t_0, "s")
 
     f2, ((ax21, ax22), (ax23, ax24)) = plt.subplots(2, 2, sharex='col', sharey='row')
     ax21.imshow(np.array(img_t))
@@ -150,22 +155,89 @@ if __name__ == '__main__':
     ax22.set_title("Observed image")
     ax23.imshow(img_dn.data.cpu().mul_(255).numpy().reshape(512, 512))
     ax23.set_title("Denoised image")
+    # Save results if specified so:
+    if args.save_flag:
+        f = open('barbara_noised.png', 'wb')
+        w1 = png.Writer(512, 512, greyscale=True)
+        w1.write(f, np.array(tensor2pil(img_obs_t.data.cpu())))
+        f.close()
+        f_res = open('barbara_denoised.png', 'wb')
+        w1.write(f_res, np.array(tensor2pil(img_dn.data.cpu())).reshape(512, 512))
+        f_res.close()
+
+    # plt.figure()
+    # res = ForwardWeightedGradient().forward(x, w)
+    # plt.imshow(res.data[0].cpu().numpy())
+    # plt.title("Gradient wrt x")
+    # plt.colorbar()
+    #
+    # plt.figure()
+    # plt.imshow(g_ref.data[0].cpu().numpy())
+    # plt.title("Gradient WRT x")
+    #
+    # plt.figure()
+    # plt.imshow(w.data[1].cpu().numpy())
+    # plt.title("Gradient wrt y")
+    # plt.colorbar()
 
 
-    plt.figure()
-    res = ForwardWeightedGradient().forward(x, w)
-    plt.imshow(res.data[0].cpu().numpy())
-    plt.title("Gradient wrt x")
-    plt.colorbar()
+    # Test image
+    img_t = Image.open("images/image_Boats512.png")
+    h, w1 = img_t.size
+    img_ref_t = Variable(pil2tensor(img_t).type(dtype))
+    noise_2 = torch.ones(img_ref_t.size())
+    noise_2 = Variable(noise_2.normal_(0.0, sigma_n).type(dtype))
+    img_obs_t = img_ref_t + noise
+    t_0 = time.time()
+    img_dn = net.forward(img_obs_t)
+    t_1 = time.time()
+    print("Elapsed time: ", t_1 - t_0, "s")
 
-    plt.figure()
-    plt.imshow(g_ref.data[0].cpu().numpy())
-    plt.title("Gradient WRT x")
+    f2, ((ax21, ax22), (ax23, ax24)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    ax21.imshow(np.array(img_t))
+    ax21.set_title("Reference image")
+    ax22.imshow(img_obs_t.data.cpu().mul_(255).numpy().reshape(512, 512))
+    ax22.set_title("Observed image")
+    ax23.imshow(img_dn.data.cpu().mul_(255).numpy().reshape(512, 512))
+    ax23.set_title("Denoised image")
+    # Save results if specified so:
+    if args.save_flag:
+        f = open('boats_noised.png', 'wb')
+        w1 = png.Writer(512, 512, greyscale=True)
+        w1.write(f, np.array(tensor2pil(img_obs_t.data.cpu())))
+        f.close()
+        f_res = open('boats_denoised.png', 'wb')
+        w1.write(f_res, np.array(tensor2pil(img_dn.data.cpu())).reshape(512, 512))
+        f_res.close()
 
-    plt.figure()
-    plt.imshow(w.data[1].cpu().numpy())
-    plt.title("Gradient wrt y")
-    plt.colorbar()
+    # Test image
+    img_t = Image.open("images/image_Lake512.png")
+    h, w1 = img_t.size
+    img_ref_t = Variable(pil2tensor(img_t).type(dtype))
+    noise_2 = torch.ones(img_ref_t.size())
+    noise_2 = Variable(noise_2.normal_(0.0, sigma_n).type(dtype))
+    img_obs_t = img_ref_t + noise
+    t_0 = time.time()
+    img_dn = net.forward(img_obs_t)
+    t_1 = time.time()
+    print("Elapsed time: ", t_1 - t_0, "s")
+
+    f2, ((ax21, ax22), (ax23, ax24)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    ax21.imshow(np.array(img_t))
+    ax21.set_title("Reference image")
+    ax22.imshow(img_obs_t.data.cpu().mul_(255).numpy().reshape(512, 512))
+    ax22.set_title("Observed image")
+    ax23.imshow(img_dn.data.cpu().mul_(255).numpy().reshape(512, 512))
+    ax23.set_title("Denoised image")
+    # Save results if specified so:
+    if args.save_flag:
+        f = open('lake_noised.png', 'wb')
+        w1 = png.Writer(512, 512, greyscale=True)
+        w1.write(f, np.array(tensor2pil(img_obs_t.data.cpu())))
+        f.close()
+        f_res = open('lake_denoised.png', 'wb')
+        w1.write(f_res, np.array(tensor2pil(img_dn.data.cpu())).reshape(512, 512))
+        f_res.close()
 
     # Plot loss
     plt.figure()
